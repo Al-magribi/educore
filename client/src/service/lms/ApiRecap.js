4c19e41f8e012fd0cb109d6741b7751fdcfa2263
Al-magribi <almagribi.appdev@gmail.com>
2026-02-11 14:50:23 +0700

    Added recap scores

---
+++ client/src/service/lms/ApiRecap.js
@@ -0,0 +1,26 @@
+﻿import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const ApiRecap = createApi({
+  reducerPath: "ApiRecap",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/lms/recap" }),
+  tagTypes: ["RecapAttendance", "RecapScoreMonthly"],
+  endpoints: (builder) => ({
+    getAttendanceRecap: builder.query({
+      query: ({ subjectId, classId, semester, month }) =>
+        `/attendance?subject_id=${subjectId || ""}&class_id=${
+          classId || ""
+        }&semester=${semester || ""}&month=${month || ""}`,
+      providesTags: ["RecapAttendance"],
+    }),
+    getScoreMonthlyRecap: builder.query({
+      query: ({ subjectId, classId, semester, month }) =>
+        `/score-monthly?subject_id=${subjectId || ""}&class_id=${
+          classId || ""
+        }&semester=${semester || ""}&month=${month || ""}`,
+      providesTags: ["RecapScoreMonthly"],
+    }),
+  }),
+});
+
+export const { useGetAttendanceRecapQuery, useGetScoreMonthlyRecapQuery } =
+  ApiRecap;
